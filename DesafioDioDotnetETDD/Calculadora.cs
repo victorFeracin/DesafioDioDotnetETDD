30114f36c551fc13798f81d3e0369542fc0ccd2c
victorFeracin <victorhugo.feracin@gmail.com>
2025-06-20 18:37:28 -0300

    Add project files.

---
+++ DesafioDioDotnetETDD/Calculadora.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioDioDotnetETDD
+{
+    public class Calculadora
+    {
+        public int Somar(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int Subtrair(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int Multiplicar(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public int Dividir(int num1, int num2)
+        {
+            return 0;
+        }
+
+        public List<string> Historico()
+        {
+            return new List<string>();
+        }
+    }
+}
---
+++ DesafioDioDotnetETDDTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using DesafioDioDotnetETDD;
+
+namespace DesafioDioDotnetETDDTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Calculadora calc = new Calculadora();
+        }
+    }
+}
